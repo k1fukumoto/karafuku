099602e26ec6c77ce8d5b75998bd3dd40451b6c6
Kaoru Fukumoto <k1fukumoto@gmail.com>
2017-06-26 21:59:32 +0900

    extract bukken no

--- reng/parser/athome/script/main.py
+++ reng/parser/athome/script/main.py
@@ -1,28 +1,29 @@
 from bs4 import BeautifulSoup
+import re
 
 # import requests
-# r = requests.get("http://www.athome.co.jp/tochi/tokyo/list")
+# r = requests.get("http://www.athome.co.jp/tochi/tokyo/list/page")
 #              headers={'User-Agent': user_agent})
 
 import sys,codecs
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout)
 
 with codecs.open('athome.html','r','utf-8') as f:
-    soup = BeautifulSoup(f.read(),"html.parser")
+    soup = BeautifulSoup(f.read(),"lxml")
 
-for p in soup.find_all('div',attrs={'class':'object-detail'}):
-    row = []
-
+for p in soup.find_all('div',attrs={'data-bukken-no':True}):
+    cols = [p['data-bukken-no']]
+    cnt = 0
     try:
         for location in p.find('li',attrs={'class':'info-trans'}).find_all('span'):
-            row.append(u' '.join(location.text.strip().split()))
-        row.append(p.find('li',attrs={'class':'info-time'}).text.strip())
-        row.append(p.find('li',attrs={'class':'info-price'}).find('span').text)
-        row.append(p.find('li',attrs={'class':'info-floor'}).find('span').text)
+            cols.append(' '.join(location.text.strip().split()))
+        cols.append(p.find('li',attrs={'class':'info-time'}).text.strip())
+        cols.append(p.find('li',attrs={'class':'info-price'}).find('span').text)
+        cols.append(p.find('li',attrs={'class':'info-floor'}).find('span').text)
         for ratio in p.find('li',attrs={'class':'info-percent'}).find_all('span'):
-            row.append(ratio.text)
+            cols.append(ratio.text)
     except:
-        True
-#        print u"PARSE ERROR:" + id
+        print "PARSE ERROR:" + str(cnt)
 
-    print u','.join(row)
+    cnt += 1
+    print ','.join(cols)
