a0b92ab39a54ab4ac8b50dc4a02cc30a86490b40
EC2 Default User <ec2-user@ip-172-31-16-159.ap-northeast-1.compute.internal>
2017-07-01 13:11:31 +0000

    filter first checkin

---
+++ reng/scraper/athome/filter/main.py
@@ -0,0 +1,16 @@
+import sys
+import csv
+
+def print_line(s):
+    sys.stdout.buffer.write(bytes(s+'\n','utf-8'))
+
+with open(sys.argv[1],encoding='utf-8') as f:
+    reader = csv.DictReader(f,fieldnames=['id','station','line','addr',
+                                          'tts','price','floor','ratio1','ratio2','url'])
+    for row in reader:
+        if(row['tts'] != "-" and int(row['tts']) <= 10 and
+           row['floor'] != "-" and float(row['floor'].replace(',','')) >=100 and
+           row['ratio2'] != "-" and int(row['ratio2'].replace('%','')) >=200 and
+           row['addr'] != "-" and (row['addr'].find('区') >=0 or row['addr'].find('ＪＲ中央線') >=0)):
+            print_line(row['url'])
+
