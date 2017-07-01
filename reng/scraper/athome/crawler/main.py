aea002d991756f3cece79a3c0c21099364e839a5
EC2 Default User <ec2-user@ip-172-31-16-159.ap-northeast-1.compute.internal>
2017-07-01 11:33:48 +0000

    update parser & crawler

--- reng/scraper/athome/crawler/main.py
+++ reng/scraper/athome/crawler/main.py
@@ -1,7 +1,15 @@
 import requests
+import re
 
-r = requests.get("http://www.athome.co.jp/tochi/tokyo/list/page1")
-with open('athome.html','wb') as fd:
-    for chunk in r.iter_content(chunk_size=128):
-        fd.write(chunk)
+URLBASE = "http://www.athome.co.jp/tochi/tokyo/list/page{}"
+
+for i in range(1,11):
+    url = URLBASE.format(i)
+    print ("Get {}...".format(url))
+    r = requests.get(url)
+
+    fname = url.replace('/','_') + '.html'
+    with open(fname,'wb') as fd:
+        for chunk in r.iter_content(chunk_size=128):
+            fd.write(chunk)
 
--- reng/scraper/athome/parser/main.py
+++ reng/scraper/athome/parser/main.py
@@ -1,22 +1,63 @@
 from bs4 import BeautifulSoup
 import sys
+import re
 
-# with open('athome.html','rb') as fd:
 soup = BeautifulSoup(sys.stdin.buffer.read(),"lxml")
 
 cnt = 0
 for p in soup.find_all('div',attrs={'data-bukken-no':True}):
     cols = [p['data-bukken-no']]
     try:
-        for location in p.find('li',attrs={'class':'info-trans'}).find_all('span'):
-            cols.append(' '.join(location.text.strip().split()))
-        cols.append(p.find('li',attrs={'class':'info-time'}).text.strip())
-        cols.append(p.find('li',attrs={'class':'info-price'}).find('span').text)
-        cols.append(p.find('li',attrs={'class':'info-floor'}).find('span').text)
+        locs = p.find('li',attrs={'class':'info-trans'}).find_all('span')
+
+        # train line & station
+        m = re.search(r'(.+)\/(.+)',locs[0].text)
+        if m:
+            cols.append(m.group(1))
+            cols.append(m.group(2))
+        else:
+            cols.append('-')
+            cols.append(locs[0].text)
+        # address
+        cols.append(' '.join(locs[1].text.strip().split()))
+
+        # time to station
+        m = re.search(r'(\d+)',p.find('li',attrs={'class':'info-time'}).text)
+        if m:
+            cols.append(m.group(1))
+        else:
+            cols.append('-')
+
+        # price
+        price = p.find('li',attrs={'class':'info-price'}).find('span').text
+        m1 = re.search(r'(\d)[^\d]([\d,]+)',price) # more than 1 okuen
+        m2 = re.search(r'([\d,]+)',price)
+        if m1:
+            cols.append(m1.group(1)+m1.group(2))
+        elif m2:
+            cols.append(m2.group(1))
+        else:
+            cols.append('-')
+
+        # floor space
+        m = re.search(r'([\d,\.]+)',p.find('li',attrs={'class':'info-floor'}).find('span').text)
+#        cols.append(p.find('li',attrs={'class':'info-floor'})
+        if m:
+            cols.append(m.group(1))
+        else:
+            cols.append('-')
+
+        # land/capacity ratios
         for ratio in p.find('li',attrs={'class':'info-percent'}).find_all('span'):
             cols.append(ratio.text)
-    except:
-        print("PARSE ERROR AT LINE " + str(cnt))
+
+        # link to property
+        cols.append("https://www.athome.co.jp/tochi/{}".format(cols[0]))
+        
+    except Exception as e:
+        print("PARSE ERROR AT LINE " + str(e))
+
+    cols = map(lambda x: '"{}"'.format(x), cols)
 
     cnt = cnt + 1
     s = u','.join(cols) + u'\n'
